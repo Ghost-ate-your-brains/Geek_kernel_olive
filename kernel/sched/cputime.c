ce4ffb1d99e948a8d156e6cab0966a379ddc5ef9
Rabin Vincent <rabinv@axis.com>
2017-12-13 11:11:16 +0100

    sched: fix softirq time accounting
    softirq time accounting is broken on v4.9.x if ksoftirqd runs.
    
    With
    	CONFIG_IRQ_TIME_ACCOUNTING=y
    	# CONFIG_VIRT_CPU_ACCOUNTING_GEN is not set
    
    this test code:
    
    	struct tasklet_struct tasklet;
    
    	static void delay_tasklet(unsigned long data)
    	{
    		udelay(10);
    		tasklet_schedule(&tasklet);
    	}
    
    	tasklet_init(&tasklet, delay_tasklet, 0);
    	tasklet_schedule(&tasklet);
    
    results in:
    
    $ while :; do grep cpu0 /proc/stat; done
    cpu0 5 0 80 25 16 107 1 0 0 0
    cpu0 5 0 80 25 16 107 0 0 0 0
    cpu0 5 0 80 25 16 107 0 0 0 0
    cpu0 5 0 80 25 16 107 0 0 0 0
    cpu0 5 0 81 25 16 107 0 0 0 0
    cpu0 5 0 81 25 16 107 1 0 0 0
    cpu0 5 0 81 25 16 108 18446744073709551615 0 0 0
    cpu0 5 0 81 25 16 108 18446744073709551615 0 0 0
    cpu0 5 0 81 25 16 108 18446744073709551615 0 0 0
    cpu0 5 0 81 25 16 108 0 0 0 0
    cpu0 6 0 81 25 16 108 0 0 0 0
    cpu0 6 0 81 25 16 108 0 0 0 0
    
    As can be seen, the softirq numbers are totally bogus.
    
    When ksoftirq is running, irqtime_account_process_tick() increments
    cpustat[CPUSTAT_SOFTIRQ].  This causes the "nsecs_to_cputime64(irqtime)
    - cpustat[CPUSTAT_SOFTIRQ]" calculation in irqtime_account_update() to
    underflow the next time a softirq is handled leading to the above
    values.
    
    The underflow bug was added by 57430218317e5b280 ("sched/cputime: Count
    actually elapsed irq & softirq time").
    
    But ksoftirqd accounting was wrong even in earlier kernels.  In earlier
    kernels, after a ksoftirq run, the kernel would simply stop accounting
    softirq time spent outside of ksoftirqd until that (accumulated) time
    exceeded the time for which ksofirqd previously had run.
    
    Fix both the underflow and the wrong accounting by using a counter
    specifically for the non-ksoftirqd softirq case.
    
    This code has been fixed in current mainline by a499a5a14db
    ("sched/cputime: Increment kcpustat directly on irqtime account") [note
    also the followup 25e2d8c1b9e327e ("sched/cputime: Fix ksoftirqd cputime
    accounting regression")], but that patch is a part of the many changes
    for eliminating of cputime_t so it does not seem suitable for backport.
    
    Change-Id: I231b4e95df1201119360dee44e6d0847b2795ac5
    Signed-off-by: Rabin Vincent <rabinv@axis.com>
    Patch-mainline: linux-kernel @ 13 Dec 2017 11:11:16
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- kernel/sched/cputime.c
+++ kernel/sched/cputime.c
@@ -94,8 +94,22 @@ EXPORT_SYMBOL_GPL(irqtime_account_irq);
 
 static cputime_t irqtime_tick_accounted(cputime_t maxtime)
 {
-	struct irqtime *irqtime = this_cpu_ptr(&cpu_irqtime);
-	cputime_t delta;
+	u64 *cpustat = kcpustat_this_cpu->cpustat;
+	u64 base = cpustat[idx];
+	cputime_t irq_cputime;
+
+	if (idx == CPUTIME_SOFTIRQ)
+		base = kcpustat_this_cpu->softirq_no_ksoftirqd;
+
+	irq_cputime = nsecs_to_cputime64(irqtime) - base;
+	irq_cputime = min(irq_cputime, maxtime);
+	cpustat[idx] += irq_cputime;
+
+	if (idx == CPUTIME_SOFTIRQ)
+		kcpustat_this_cpu->softirq_no_ksoftirqd += irq_cputime;
+
+	return irq_cputime;
+}
 
 	delta = nsecs_to_cputime(irqtime->tick_delta);
 	delta = min(delta, maxtime);
